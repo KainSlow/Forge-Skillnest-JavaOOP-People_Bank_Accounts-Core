0b8cf483752622c136fa101676813610456020b5
KainSlow <jorge_alfonso3@hotmail.com>
2025-04-17 01:31:57 -0400

    Updated: -Formatted -Fixed Typo

--- clases/CuentaBancaria.java
+++ clases/CuentaBancaria.java
@@ -1,54 +1,66 @@
 package clases;
 
 import java.util.ArrayList;
-import clases.Persona;
 
-public class CuentaBancaria{
-    
-    public static ArrayList<CuentaBancaria> listaDeCuentasBancarias = new ArrayList<CuentaBancaria>();
+public class CuentaBancaria {
+
+    public static final ArrayList<CuentaBancaria> listaDeCuentasBancarias = new ArrayList<>();
 
     private Persona titular;
     private Double saldo;
     private int numeroCuenta;
-    
-    public CuentaBancaria(Persona titular, Double saldo){
+
+    public CuentaBancaria(Persona titular, Double saldo) {
         this.titular = titular;
         this.saldo = saldo;
-        numeroCuenta =  100000 + (int)Math.floor(Math.random() * (999999.0 - 100000.0));
+        numeroCuenta = 100000 + (int) Math.floor(Math.random() * (999999.0 - 100000.0));
 
         CuentaBancaria.listaDeCuentasBancarias.add(this);
     }
 
-    public Persona getTitular(){return titular;}
-    public void setTitular(Persona p){ titular = p;}
+    public Persona getTitular() {
+        return titular;
+    }
 
-    public Double getSaldo(){return saldo;}
-    public void setSaldo(Double s){saldo = s;}
+    public void setTitular(Persona p) {
+        titular = p;
+    }
 
-    public int getNumeroCuenta(){return numeroCuenta;}
-    public void setNumeroCuenta(int n){
-        if(n < 100000 || n > 999999){
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double s) {
+        saldo = s;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int n) {
+        if (n < 100000 || n > 999999) {
             System.out.println("Número no válido");
             return;
         }
         numeroCuenta = n;
     }
 
-    public void depositar(Double monto){
+    public void depositar(Double monto) {
         saldo += monto;
     }
 
-    public void retirar(Double monto){
-        
-        if (saldo - monto < 0.0){
-            System.out.println("No hay suficiente salto para retirar el monto indicado.");
+    public void retirar(Double monto) {
+
+        if (saldo - monto < 0.0) {
+            System.out.println("No hay suficiente saldo para retirar el monto indicado.");
             return;
         }
 
         saldo -= monto;
     }
 
-    public void despliegaInformacion(){
+    public void despliegaInformacion() {
         System.out.println("Datos de la cuenta:");
         System.out.println("Numero de cuenta: " + String.valueOf(numeroCuenta));
         System.out.println("Saldo: " + String.valueOf(saldo));
@@ -56,8 +68,8 @@ public class CuentaBancaria{
         System.out.println("");
     }
 
-    public static void imprimeInformacionDeTodasLasCuentas(){
-        for(CuentaBancaria cuenta : listaDeCuentasBancarias){
+    public static void imprimeInformacionDeTodasLasCuentas() {
+        for (CuentaBancaria cuenta : listaDeCuentasBancarias) {
             cuenta.despliegaInformacion();
         }
     }
